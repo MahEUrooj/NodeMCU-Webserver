e4a5737363ea7e244d22051fa3c83803952002ee
MahEUrooj <murooj158@gmail.com>
2022-06-21 20:46:32 +0500

    Add files via upload

---
+++ NodeMCU_Webserver.ino
@@ -0,0 +1,34 @@
+#include <ESP8266WiFi.h>
+
+WiFiClient client;
+WiFiServer server(80);
+
+#define led D5
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin("Mooazam", "mooazam123");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
+  }
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  pinMode(led, OUTPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+    String request =  client.readStringUntil('\n');
+    Serial.println(request);
+  }
+}
